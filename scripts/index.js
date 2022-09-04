27a83d6af9632762c79c75c332e98958662b29e4
itz-dude <its.kaydem@gmail.com>
2022-09-04 12:23:59 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Sept 19, 2022',
+  birthdate: 'Sep 19, 2022',
   name: "Tushar"
 };
 
